9ae3777a5d81b9ef40cef0cf9bda219c6fc8a696
piotrv1001 <piotrv1001@interia.pl>
2023-03-05 08:41:24 +0100

    Added communication between squares

--- src/components/square/square.component.ts
+++ src/components/square/square.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
+import { SquareService } from './../../app/services/square.service';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, OnDestroy, AfterViewInit, OnInit, Renderer2 } from '@angular/core';
 import { Square } from 'src/model/square';
 import { fromEvent, Subscription } from 'rxjs';
 
@@ -7,27 +8,87 @@ import { fromEvent, Subscription } from 'rxjs';
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.scss']
 })
-export class SquareComponent implements AfterViewInit, OnDestroy {
+export class SquareComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() square?: Square;
   @ViewChild('squareRef') squareImgRef?: ElementRef;
 
   @Output() squareClick = new EventEmitter<Square>();
 
+  isLegalMove: boolean = false;
+  private otherSquareClickSub?: Subscription;
+  private legalMoveSub?: Subscription;
   private squareClickSub?: Subscription;
+  private squareMouseOverSub?: Subscription;
+  private squareMouseOutSub?: Subscription;
+
+  constructor(
+    private squareService: SquareService,
+    private renderer: Renderer2) {}
+
+  get squareNative(): any {
+    return this.squareImgRef?.nativeElement;
+  }
+
+  ngOnInit(): void {
+    this.otherSquareClickSub = this.squareService.getSquareClickObservable()
+      .subscribe(clickedSquare => {
+        // check if we clicked on a different square
+        if(this.square !== clickedSquare) {
+          if(this.square?.occupiedBy != null) {
+            const classToRemove = (this.square?.isWhite) ? 'clicked-white-square' : 'clicked-dark-square';
+            this.renderer.removeClass(this.squareNative, classToRemove);
+          }
+        }
+    });
+    this.legalMoveSub = this.squareService.getLegalMoveObservable()
+      .subscribe(legalMoves => {
+        // check if this square is in the array of legal moves
+        if(this.square && legalMoves.includes(this.square)) {
+          this.isLegalMove = true;
+        } else {
+          this.isLegalMove = false;
+        }
+      });
+  }
 
   ngAfterViewInit(): void {
-    const squareNative = this.squareImgRef?.nativeElement;
-    if(squareNative) {
-      const squareClick$ = fromEvent(squareNative, 'click');
+    if(this.squareNative) {
+      const squareClick$ = fromEvent(this.squareNative, 'click');
       this.squareClickSub = squareClick$.subscribe(() => {
+        if(this.square?.occupiedBy != null) {
+          const classToAdd = (this.square?.isWhite) ? 'clicked-white-square' : 'clicked-dark-square';
+          this.renderer.addClass(this.squareNative, classToAdd);
+        }
         this.squareClick.emit(this.square);
+      });
+      const squareMouseOver$ = fromEvent(this.squareNative, 'mouseover');
+      this.squareMouseOverSub = squareMouseOver$.subscribe(() => {
+        if(this.square?.occupiedBy != null) {
+          document.body.style.cursor = 'pointer';
+        }
+      });
+      const squareMouseOut$ = fromEvent(this.squareNative, 'mouseout');
+      this.squareMouseOutSub = squareMouseOut$.subscribe(() => {
+        if(this.square?.occupiedBy != null) {
+          document.body.style.cursor = 'default';
+        }
       });
     }
   }
 
   ngOnDestroy(): void {
-    if(this.squareClickSub) {
-      this.squareClickSub.unsubscribe();
+    this.removeSubs(
+      this.squareClickSub,
+      this.squareMouseOverSub,
+      this.squareMouseOutSub,
+      this.otherSquareClickSub,
+      this.legalMoveSub
+    )
+  }
+
+  private removeSubs(...subs: (Subscription | undefined)[]): void {
+    for(const sub of subs) {
+      sub?.unsubscribe();
     }
   }
 
