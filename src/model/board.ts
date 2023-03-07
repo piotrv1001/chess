629e773d8b115be7c9bef2da5b3446499725a446
PiotrV <piotrv1001@gmail.com>
2023-03-07 20:04:16 +0100

    Handling checks - in progress

--- src/model/board.ts
+++ src/model/board.ts
@@ -15,6 +15,7 @@ export class Board {
   isCheckMate: boolean = false;
   constructor() {
     this.squares = this.initSquares();
+    this.checkIfCheckmate(true);
   }
   private checkIfCheckmate(isWhite: boolean): boolean {
     for(const square of this.squares) {
@@ -39,15 +40,74 @@ export class Board {
   }
   // check if the king of the player who just made a move is in check -> if yes, that move is illegal
   isKingInCheck(): boolean {
-    const isWhite = this.lastMove?.piece.isWhite;
+    const isWhite = this.lastMove?.piece.isWhite ?? false;
     const kingSquare = this.squares.find(square => square.occupiedBy instanceof King && square.occupiedBy.isWhite === isWhite);
     if(kingSquare) {
-      const legalMoves = this.lastMove?.piece.checkLegalMoves(this.squares);
-      if(legalMoves) {
-        return legalMoves.find(move => move.endingPos === kingSquare) !== undefined;
+      let row = kingSquare.position.row;
+      let col = kingSquare.position.column;
+      const left = this.checkDirection(isWhite, row, col, true, false);
+      const right = this.checkDirection(isWhite, row, col, true, true);
+      const top = this.checkDirection(isWhite, row, col, false, false, true);
+      const bottom = this.checkDirection(isWhite, row, col, false, false, false);
+      const topRight = this.checkDirectionDiagonally(isWhite, row, col, true, true);
+      const topLeft = this.checkDirectionDiagonally(isWhite, row, col, false, true);
+      const bottomRight = this.checkDirectionDiagonally(isWhite, row, col, true, false);
+      const bottomLeft = this.checkDirectionDiagonally(isWhite, row, col, false, false);
+      return left || right || top || bottom || topRight || topLeft || bottomRight || bottomLeft;
+    }
+    return false;
+    // const isWhite = this.lastMove?.piece.isWhite;
+    // const kingSquare = this.squares.find(square => square.occupiedBy instanceof King && square.occupiedBy.isWhite === isWhite);
+    // if(kingSquare) {
+    //   const legalMoves = this.lastMove?.piece.checkLegalMoves(this.squares);
+    //   if(legalMoves) {
+    //     return legalMoves.find(move => move.endingPos === kingSquare) !== undefined;
+    //   }
+    // }
+    // return false;
+  }
+  private checkDirection(isWhite: boolean, row: number, col: number, horizontal: boolean, right: boolean = false, top: boolean = false): boolean {
+    let offset = 0;
+    let condition = false;
+    if(horizontal) {
+      offset = right ? 1 : -1
+      condition = right ? (col < 8) : (col > 1);
+    } else {
+      offset = top ? -1 : 1;
+      condition = top ? (row > 1) : (row < 8);
+    }
+    while(condition) {
+      if(horizontal) {
+        row += offset;
+      } else {
+        col += offset;
+      }
+      const foundSquare = this.findSquare(row, col);
+      if(foundSquare && foundSquare.occupiedBy != null) {
+        const isEnemy = foundSquare.occupiedBy.isWhite !== isWhite;
+        return isEnemy && (foundSquare.occupiedBy instanceof Queen || foundSquare.occupiedBy instanceof Rook);
       }
     }
     return false;
+  }
+  private checkDirectionDiagonally(isWhite: boolean, row: number, col: number, right: boolean, top: boolean): boolean {
+    const offsetCol = right ? 1 : -1;
+    const offsetRow = top ? -1 : 1;
+    const conditionCol = right ? (col < 8) : (col > 1);
+    const conditionRow = top ? (col > 1) : (col < 8);
+    while(conditionCol && conditionRow) {
+      row += offsetRow;
+      col += offsetCol;
+      const foundSquare = this.findSquare(row, col);
+      if(foundSquare && foundSquare.occupiedBy != null) {
+        const isEnemy = foundSquare.occupiedBy.isWhite !== isWhite;
+        return isEnemy && (foundSquare.occupiedBy instanceof Queen || foundSquare.occupiedBy instanceof Bishop);
+      }
+    }
+    return false;
+  }
+  private findSquare(row: number, col: number): Square | undefined {
+    return this.squares.find(square => square.position.row === row && square.position.column === col);
   }
   private initSquares(): Square[] {
     const initialSquares: Square[] = [];
